28a39f5a3744a8bdb8cd672e4c42e1d783323fec
aiueo4u <aiueo4u@gmail.com>
2017-10-05 11:43:02 +0900

    add link

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -17,6 +17,11 @@ const Home = ({ nickname }) => (
       </Link>
     </div>
     <div>
+      <Link to="/tables">
+        <RaisedButton label="Table list" />
+      </Link>
+    </div>
+    <div>
       <RaisedButton label="Log out" onTouchTap={clearAllData} />
     </div>
   </div>
