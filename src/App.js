0ce5daed155eea721f7b69f2104cce56589f6add
aiueo4u <aiueo4u@gmail.com>
2017-10-07 16:41:45 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
         'margin': 0,
       }}>
         <Router>
-          <div style={{ 'background': '#884444' }}>
+          <div>
             <PrivateRoute exact path="/" component={Home} />
             <Route path="/login" component={Login} />
             <PrivateRoute path="/newTable" component={Lobby} />
--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
+import wallpaperImage from './wallpaper_001.jpg';
 
 const clearAllData = () => {
   localStorage.clear();
@@ -9,20 +10,40 @@ const clearAllData = () => {
   window.location = '/';
 }
 
+const homeStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  position: 'fixed',
+  height: '50%',
+  width: '80%',
+  textAlign: 'center',
+  margin: 'auto',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  background: 'rgba(200, 200, 200, 0.3)',
+  borderRadius: '30px',
+}
+
 const Home = ({ nickname }) => (
   <div>
-    <div>
-      <Link to="/newTable">
-        <RaisedButton label="Create new table" />
-      </Link>
-    </div>
-    <div>
-      <Link to="/tables">
-        <RaisedButton label="Table list" />
-      </Link>
-    </div>
-    <div>
-      <RaisedButton label="Log out" onTouchTap={clearAllData} />
+    <img src={wallpaperImage} style={{ position: 'fixed', height: '100vh', filter: 'blur(2px)' }} />
+    <div style={homeStyle}>
+      <div style={{ margin: '5vh' }}>
+        <Link to="/newTable">
+          <RaisedButton label="new table" primary={true} />
+        </Link>
+      </div>
+      <div style={{ margin: '5vh' }}>
+        <Link to="/tables">
+          <RaisedButton label="Table list" primary={true} />
+        </Link>
+      </div>
+      <div style={{ margin: '5vh' }}>
+        <RaisedButton label="Log out" onTouchTap={clearAllData} primary={true} />
+      </div>
     </div>
   </div>
 )
