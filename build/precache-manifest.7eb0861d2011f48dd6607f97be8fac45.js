bdfcec17898da08f8bb5c98aa696e35720d670dd
Masahiro Yamada <aiueo4u@gmail.com>
2020-03-22 22:59:24 +0900

    build

---
+++ build/precache-manifest.7eb0861d2011f48dd6607f97be8fac45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c457384c85a8e4c525578b73ad1b1872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf363f9d5cccea84a7a",
+    "url": "/static/css/main.79cd1858.chunk.css"
+  },
+  {
+    "revision": "b678892ddb083c61b56d",
+    "url": "/static/js/2.e3b618d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e3b618d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf363f9d5cccea84a7a",
+    "url": "/static/js/main.2b658256.chunk.js"
+  },
+  {
+    "revision": "e5c6afafb592122e4235",
+    "url": "/static/js/runtime-main.b37a5fb5.js"
+  },
+  {
+    "revision": "08a69d65eb56b7e03d5db65d3aa7fe04",
+    "url": "/static/media/back.08a69d65.png"
+  },
+  {
+    "revision": "a30df6391e361aace257e245da207f77",
+    "url": "/static/media/wallpaper_001.a30df639.jpg"
+  }
+]);
